4b455c6bc5fcef8665faaec8597bf4bc429e01c8
Shaina C <scailles23@gmail.com>
2021-01-05 06:40:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+class Dog
+  
+  @@all = []
+  
+  def initialize(name)
+    @name=name
+    save
+  end
+  
+  def name
+    @name
+  end
+  
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+end
